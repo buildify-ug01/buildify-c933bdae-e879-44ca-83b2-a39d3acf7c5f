bdec6cb107e7f8912ebe26b7fd4182aba15c94ad
Buildify <buildify-bot@buildify.dev>
2025-10-23 08:49:56 +0000

    Updated project files

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from 'next-themes';
 import { Toaster } from 'sonner';
 
@@ -11,35 +10,23 @@ import TemplatesPage from './pages/TemplatesPage';
 import MyPostersPage from './pages/MyPostersPage';
 import ProfilePage from './pages/ProfilePage';
 
-// Create a client
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1000 * 60 * 5, // 5 minutes
-      retry: 1,
-    },
-  },
-});
-
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
-        <Router>
-          <div className="min-h-screen bg-background">
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/editor" element={<EditorPage />} />
-              <Route path="/editor/:id" element={<EditorPage />} />
-              <Route path="/templates" element={<TemplatesPage />} />
-              <Route path="/my-posters" element={<MyPostersPage />} />
-              <Route path="/profile" element={<ProfilePage />} />
-            </Routes>
-          </div>
-          <Toaster position="top-center" richColors />
-        </Router>
-      </ThemeProvider>
-    </QueryClientProvider>
+    <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+      <Router>
+        <div className="min-h-screen bg-background">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/editor" element={<EditorPage />} />
+            <Route path="/editor/:id" element={<EditorPage />} />
+            <Route path="/templates" element={<TemplatesPage />} />
+            <Route path="/my-posters" element={<MyPostersPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+          </Routes>
+        </div>
+        <Toaster position="top-center" richColors />
+      </Router>
+    </ThemeProvider>
   );
 }
 
